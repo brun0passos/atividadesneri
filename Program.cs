0aeabd749712be6a02fdc7c58cf8150c5ad221c3
brun0passos <bruno.passos.bueno@gmail.com>
2025-05-19 20:45:16 -0300

    Finalizar Programa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+{
+   
+    {
+        Console.Write("Digite um número: 15");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} é par");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} é ímpar");
+        }
+    }
+}
+
